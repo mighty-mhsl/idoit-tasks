8030d4cd1b5f121fa2776736114d396edd2e4cb0
Vladislav Mishustin <vladislav.mishustin@onsolve.com>
2021-02-06 14:25:43 +0300

    lesson 3 solution from user: mhsl

--- src/main/java/com/idoit/App.java
+++ src/main/java/com/idoit/App.java
@@ -1,8 +1,10 @@
 package com.idoit;
 
+import java.time.LocalDateTime;
+
 public class App {
 
     public static void main(String[] args) {
-
+        System.out.println("I do it! " + LocalDateTime.now().getYear());
     }
 }
