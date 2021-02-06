b70567950df2af84521ff7909369b4b5ef4bbca6
Vladislav Mishustin <vladislav.mishustin@onsolve.com>
2021-02-06 12:49:42 +0300

    block 1 lesson 3 solution

--- src/main/java/com/idoit/App.java
+++ src/main/java/com/idoit/App.java
@@ -1,8 +1,11 @@
 package com.idoit;
 
+import java.time.LocalDateTime;
+
 public class App {
 
     public static void main(String[] args) {
-
+        System.out.println("I do it!");
+        System.out.println(LocalDateTime.now().getYear()); //всегда пишет текущий год, но вы можете написать просто число
     }
 }
